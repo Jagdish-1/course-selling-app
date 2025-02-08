959f0bba42bace8afc80eef08ebac50f8b2458cd
Jagdish-1 <jagdishjdh31@gmail.com>
2025-02-09 01:19:42 +0530

    enviroment variable are set

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter} = require("./routes/admin")
-
+const {userModel , adminModel , courseModel , purchaseModel} = require("./db")
 mongoose.connect("mongodb+srv://jagdishsuthar4581:j8J8up7a0bI9R4uY@course-selling-app.dyqiw.mongodb.net/")
 
 app.use("/users" , userRouter);
